622083d246c3f8fdfa7a5e49e60a7b0bf13424d1
wilson-ye-chen <ye.chen@sydney.edu.au>
2026-04-30 20:23:40 +1000

    FIX: sine error map bias

--- wesmooth/maps.py
+++ wesmooth/maps.py
@@ -4,11 +4,11 @@ def err_map_shift(x, s=1):
     b = np.random.normal(0, s)
     return x + b
 
-def err_map_sine(x, s=2, a=0.3, k=3):
-    c = np.random.normal(0, s, size=(k, 1))
+def err_map_sine(x, a=0.3, k=3):
+    c = np.random.uniform(-1, 1, size=(k, 1))
     c = np.tile(c, (1, len(x)))
     x = np.tile(x, (k, 1))
-    y = x - a * np.sin(np.pi * (x - c)) / (np.pi)
+    y = x - a * np.sin(np.pi * (x - c)) / np.pi
     w = np.random.uniform(size=(1, k))
     w = w / np.sum(w)
     return np.dot(w, y)
